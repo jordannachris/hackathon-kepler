81794a45db2a411a1bd1d59931bd9769c73e6627
Larissa Nahan Dantas <larissanahan@hotmail.com>
2021-03-13 15:40:30 -0300

    Primeira Tentativa Style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>  
+  <v-app class="background">  
     <v-main>
       <router-view></router-view>
     </v-main>
@@ -24,5 +24,11 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+  #app {
+    background-color: #f6d2c0;
+    font-family: Roboto;
+    padding-right: 20px;
+    padding-left: 20px;
+  }
 </style>
