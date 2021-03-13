87543b04c3445266729d1a6562c9c9be42040bc4
jordannachris <jordannachris@gmail.com>
2021-03-13 16:50:04 -0300

    ultimo commit

--- src/views/OndeEncontrar.vue
+++ src/views/OndeEncontrar.vue
@@ -1,89 +1,33 @@
 <template>
   <v-container>
     <div class="text-center ondeencontrar--botaocidades">
-      <!-- <v-menu offset-y>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn color="primary" dark v-bind="attrs" v-on="on">
-            Selecione a cidade &#9660;
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item v-for="item of itens" :key="item.id">
-            <v-list-item-title @click="getCidade">{{
-              item.title
-            }}</v-list-item-title>
-          </v-list-item>
-        </v-list> -->
-      <!-- <v-list>
-          <v-list-item v-for="dado of dadosAPI" :key="dado.id">
-            <v-list-item-title>{{ dado.local.endereco }}</v-list-item-title>
-          </v-list-item>
-        </v-list> -->
-      <!-- </v-menu> -->
-
-      <!-- <tr v-for="dado of dadosAPI" :key="dado.id">
-        <td>
-          <v-avatar>
-            <img :src="dado.imagem" alt="Ovo" />
-          </v-avatar>
-          <span>{{ dado.nome }}</span>
-          <span>{{ dado.preco }}</span>
-        </td>
-      </tr> -->
-
-      <!-- <v-simple-table>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">
-                Imagem
-              </th>
-              <th class="text-left">
-                Informações
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td class="img-ovo"><img :src="dado[].imagem" /></td>
-              <td>{{ dado.nome }}</td>
-
-              <td class="img-ovo"><img :src="dado.imagem" /></td>
-              <td>{{ dado.nome }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table> -->
-
-      <v-col cols="6">
-        <v-card>
+      <v-col cols="">
+        <v-card class="card1">
           <v-img :src="dadosAPI[0].imagem" class="grey darken-4"></v-img>
           <v-card-title class="title">
-            <span>Nome: {{ dadosAPI[0].nome }}</span>
-            <span>Valor: {{ dadosAPI[0].preco }}</span>
-            <span>Valor: {{ dadosAPI[0].preco }}</span>
+            <span>{{ dadosAPI[0].nome }}</span>
+            <span>Valor R$: {{ dadosAPI[0].preco }}</span>
           </v-card-title>
 
           <v-btn>Comprar</v-btn>
         </v-card>
 
-        <v-card>
+        <v-card class="card1">
           <v-img :src="dadosAPI[1].imagem" class="grey darken-4"></v-img>
           <v-card-title class="title">
-            <span>Nome: {{ dadosAPI[1].nome }}</span>
-            <span>Valor: {{ dadosAPI[1].preco }}</span>
-            <span>Valor: {{ dadosAPI[1].preco }}</span>
+            <span> {{ dadosAPI[1].nome }}</span>
+            <span>Valor R$: {{ dadosAPI[1].preco }}</span>
           </v-card-title>
 
           <v-btn>Comprar</v-btn>
         </v-card>
 
-        <v-card>
+        <v-card class="card1">
           <v-img :src="dadosAPI[2].imagem" class="grey darken-4"></v-img>
           <v-card-title class="title">
-            <span>Nome: {{ dadosAPI[2].nome }}</span>
-            <span>Valor: {{ dadosAPI[2].preco }}</span>s
-            <span>Valor: {{ dadosAPI[2].preco }}</span>
+            <span> {{ dadosAPI[2].nome }}</span>
+            <span>Valor R$: {{ dadosAPI[2].preco }}</span>
+            
           </v-card-title>
 
           <v-btn>Comprar</v-btn>
@@ -131,6 +75,15 @@ export default {
 </script>
 
 <style scoped>
-.img-ovo {
+.card1 {
+  width: 5000px;
+  padding-bottom: 8px;
+  margin-bottom: 10px;
 }
+
+.title {
+  display: flex;
+  justify-content: center;
+}
+
 </style>
