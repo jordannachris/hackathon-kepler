bfd7156703a36d68cdf177e421c267a50cc30952
jordannachris <jordannachris@gmail.com>
2021-03-13 14:33:02 -0300

    Commit do bottom-bar

--- src/components/shared/AppTabBar.vue
+++ src/components/shared/AppTabBar.vue
@@ -1,13 +1,35 @@
 <template>
-    <div class="app-tab-bar"></div>
+  <div class="app-tab-bar">
+    <v-bottom-navigation v-model="value">
+      <v-btn value="recent">
+        <span>Recent</span>
+
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn value="favorites">
+        <span>Favorites</span>
+
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn value="nearby">
+        <span>Nearby</span>
+
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
 </template>
+  
+
 
 <script>
 export default {
-    name: 'AppTabBar',
-}
+  name: "AppTabBar",
+  data: () => ({ value: "recent" }),
+};
 </script>
 
 <style scoped>
-
 </style>
